6546e1326fcd18791455584c74d9b874b0dccd95
Showaa-06 <115485438+Showaa-06@users.noreply.github.com>
2023-01-08 00:27:38 +0900

    first commit

--- Loop/main.swift
+++ Loop/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+//色々なfor in Loop
+let japan: Array = ["Hokkaido", "Tokyo", "Osaka"]
+for Japan in japan {
+    print(Japan)
+}
+
+let score = ["Shogo": 90, "Akira": 80, "Satoru": 70]
+for Score in score {
+    print(Score.key)
+    print(Score.value)
+    
+}
+
+let value = 1..<5
+for _ in value {
+   print("4Loops")
+}
+
+
+
+
+
+
+
+
 
